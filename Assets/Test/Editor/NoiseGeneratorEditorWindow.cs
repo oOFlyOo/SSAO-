cba256a30925e5c17c95bdd4422f5fae5ad911b9
Fly <393830887@qq.com>
2020-11-22 17:38:20 +0800

    提交自己添加

---
+++ Assets/Test/Editor/NoiseGeneratorEditorWindow.cs
@@ -0,0 +1,94 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class NoiseGeneratorEditorWindow : EditorWindow
+{
+    int x;
+    int y;
+    string texName;
+    int scale = 1;
+    int step = 1;
+
+    [MenuItem("ToolsEx/生成噪声图")]
+    static void Init()
+    {
+        EditorWindow.GetWindow(typeof(NoiseGeneratorEditorWindow)).Show();
+    }
+
+    void GenerateNoiseImage(int x, int y)
+    {
+        int size = Mathf.Min(x, y);
+        Texture2D tex = new Texture2D(x, y, TextureFormat.RGB24, false);
+        Color[] pixel = new Color[x * y];
+        for (int i = 0; i < x; i = i + step)
+        {
+            pixel[i] = new Color(1, 1, 1);
+        }
+        for (int i = 0; i < x; i = i + step)
+        {
+            for (int j = 0; j < x; j = j + step)
+            {
+                var vec = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+                vec.Normalize();
+                vec = (vec + new Vector3(1, 1, 0)) * 0.5f;
+                
+                pixel[i * size + j] = new Color(vec.x, vec.y, vec.z);
+            }
+        }
+        tex.SetPixels(pixel);
+        tex.Apply();
+                      
+        File.WriteAllBytes(System.Environment.CurrentDirectory + "\\Assets\\" + texName + ".png", tex.EncodeToPNG());
+        EditorUtility.DisplayDialog("成功", "噪声图\"" + texName + "\"" + "已在Assets目录下生成！", "确定", "取消");
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        texName = EditorGUILayout.TextField("图片名: ", texName);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        y = EditorGUILayout.IntField("图片长: ", y);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        x = EditorGUILayout.IntField("图片宽: ", x);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        scale = EditorGUILayout.IntField("Scale: ", scale);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        step = EditorGUILayout.IntField("Step: ", step);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("生成"))
+        {
+            if (x < 1 || y < 1)
+            {
+                Debug.LogError("图片长或宽必须大于1");
+                return;
+            }
+            if (texName == null || texName.Length < 1)
+            {
+                Debug.LogError("请为图片命名");
+                return;
+            }
+            if (step >= x || step >= y)
+            {
+                Debug.LogError("密度不能超过图片的长或宽");
+                return;
+            }
+            else if (step < 1)
+            {
+                step = 1;
+            }
+            GenerateNoiseImage(x, y);
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Test/MatrixShader.shader
@@ -0,0 +1,66 @@
+﻿Shader "Unlit/MatrixShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+                float4 vertex1 : TEXCOORD1;
+                float4 vertex2 : TEXCOORD2;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+
+                o.vertex = mul(UNITY_MATRIX_P, mul(UNITY_MATRIX_V, mul(unity_ObjectToWorld, v.vertex)));
+                // o.vertex = mul(UNITY_MATRIX_P, mul(unity_WorldToCamera, mul(unity_ObjectToWorld, v.vertex)));
+                o.vertex1 = mul(UNITY_MATRIX_V, v.vertex);
+                o.vertex2 = mul(unity_WorldToCamera, v.vertex);
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
